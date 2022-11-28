0d3af3f2a7dde7fdd5c96ce8ea24124e619f2a2b
Ashutosh Singh <ashutosh5786@outlook.com>
2022-11-28 18:18:59 +0530

    added socket.io

--- index.js
+++ index.js
@@ -25,4 +25,29 @@ app.listen(2000, function (err) {
 	console.log("listening on http://localhost:2000");
 })
 
-///
+/// Socket IO code ###################################################################################
+
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+  res.render("Hello");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port http://localhost:3000");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user has connected with ID: " + socket.id);
+
+  socket.on("message", (data) => {
+    socket.broadcast.emit("message", data);  })
+});
+
+// SOCKET IO CLIENT CODE
+//  Hello.ejs is the client code for socket io
