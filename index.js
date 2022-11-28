d99f2b378050b459f364f6e4ec8101a576055c7e
Ashutosh Singh <ashutosh5786@outlook.com>
2022-11-28 18:31:34 +0530

    added filsesystem

--- index.js
+++ index.js
@@ -82,4 +82,48 @@ btn.addEventListener('click', function onClick(event) {
 });
 
 
-// #######################################  
+// #######################################  File system wala code to write file
+var fs = require('fs');
+
+
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "form.html"));
+});
+
+
+app.post('/data', [ 
+
+  check('name').isAlpha().withMessage('Must be alpha ony').isLength({ min: 10, max: 20 }).withMessage('Name length should be 10 to 20 characters'), 
+  
+  check('email').isEmail().isLength({ min: 10, max: 30 }).withMessage('Email length should be 10 to 30 characters'), 
+
+  check('mobile').isLength({ min: 10, max: 10 }). withMessage('Mobile number should contains 10 digits'), 
+
+  check('password').isLength({ min: 8, max: 10 }).withMessage('Password length should be 8 to 10 characters'),
+
+  oneOf([
+      check('gender').isIn(['male', 'female']).withMessage('Gender is required')]),
+
+], function(req, res) { 
+
+const errors = validationResult(req); 
+if (!errors.isEmpty()) { 
+  res.send(errors) 
+} 
+
+else { 
+      response = {  
+          email:req.body.email,  
+          name:req.body.name,
+          mobile: req.body.mobile,
+          password:req.body.password,
+          gender:req.body.gender
+         };  
+         let data = JSON.stringify(response, null, 2);
+         fs.appendFile('data.txt', data, function () { 
+          console.log('Write operation complete.');
+      });
+      res.end("successfully submitted");
+} 
+});
